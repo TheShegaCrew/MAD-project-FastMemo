e5810f7ec823c84896ef43f5aeec63da12d2779d
libranetx <tadiostsegaye7@gmail.com>
2026-01-17 15:25:48 +0300

    feat: Added Pin dialog function

--- app/src/main/java/com/example/notememo/MainActivity.java
+++ app/src/main/java/com/example/notememo/MainActivity.java
@@ -74,6 +74,55 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    private void showPinDialog() {
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
 
+        View rootView = findViewById(android.R.id.content);
+        ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setTitle("Enter PIN");
+        builder.setMessage("Please enter your PIN to access Fast Memo");
+
+        TextInputLayout pinLayout = new TextInputLayout(this);
+        TextInputEditText pinInput = new TextInputEditText(this);
+        pinInput.setInputType(android.text.InputType.TYPE_CLASS_NUMBER | android.text.InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        pinLayout.addView(pinInput);
+        pinLayout.setPadding(50, 0, 50, 0);
+
+        builder.setView(pinLayout);
+        AlertDialog pinDialog = builder.create();
+        pinDialog.setOnShowListener(d -> {
+            if (pinDialog.getWindow() != null) {
+                pinDialog.getWindow().setDimAmount(0.75f);
+                pinDialog.getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+            }
+        });
+        pinDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", (dialog, which) -> {
+            String pin = pinInput.getText().toString();
+            if (SecurityHelper.verifyPin(this, pin)) {
+                // PIN verified, continue with normal initialization
+                viewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(MemoViewModel.class);
+                initViews();
+                setupToolbar();
+                setupRecyclerView();
+                setupSearch();
+                setupCategoryFilters();
+                setupFAB();
+                observeViewModel();
+            } else {
+                Toast.makeText(this, "Incorrect PIN", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+        pinDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel", (dialog, which) -> finish());
+        pinDialog.setCancelable(false);
+        pinDialog.show();
+    }
 
 }
